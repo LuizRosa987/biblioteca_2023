3405a4aeb034275733dd1330a95cad45425a369a
Luiz Rosa <luizcarlosaparecidorosa@gmail.com>
2023-09-14 23:48:35 +0000

    salvando o arquivo

--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -10,5 +10,20 @@ import jakarta.persistence.Table;
 @Table(name="generos")
 public class Genero{
     @Id
-    
+    @GeneratedValue(strategy=GeneratedValue.IDENTITY)
+    private int id;
+    private String nome;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ app/src/main/java/application/model/GeneroRepository.java
@@ -0,0 +1,7 @@
+package application.model;
+
+import org.springframawork.data.repository.CrudRepository;
+
+public interface GeneroRepository extends CrupRepository<Genero, Interger> {
+    
+}
